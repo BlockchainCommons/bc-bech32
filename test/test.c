d2ae83ffd205d50485822d4d5e820ef4e3658ca9
Wolf McNally <wolf@wolfmcnally.com>
2020-04-20 20:58:07 -0700

    Added build system.

---
+++ test/test.c
@@ -0,0 +1,16 @@
+//
+//  test.c
+//
+//  Copyright © 2020 by Blockchain Commons, LLC
+//  Licensed under the "BSD-2-Clause Plus Patent License"
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "../src/bc-bech32.h"
+#include "test-utils.h"
+
+int main() {
+}
